cc620d36476a39c2d9cfbcf29c8d8a6c2c6d235a
luis-moguea <luis.mogueadp@gmail.com>
2023-08-07 16:28:01 -0500

    style and data filter updates

--- src/components/views/InputComp.tsx
+++ src/components/views/InputComp.tsx
@@ -18,13 +18,61 @@ const InputComp = () => {
   const [data, setData] = useState<Data[]>([]);
   const [tapKey, setTapKey] = useState(false);
   const [empty, setEmpty] = useState(false);
+  const [noData, setNoData] = useState(false);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      setInputValue(event.currentTarget.value);
+
+      if (inputValue.length === 0) {
+        setEmpty(true);
+        setNoData(false);
+        setTapKey(false);
+        setSeeAll(false);
+
+        setTimeout(() => {
+          setEmpty(false);
+        }, 3000);
+      }
+    }
+  };
 
   useEffect(() => {
+    const filter = data.filter((el) => {
+      return el.company.toLowerCase().includes(inputValue.toLowerCase());
+    });
+
+    setFilteredData(filter);
+    setTapKey(filter.length >= 1);
+  }, [inputValue, data]);
+
+  useEffect(() => {
+    setTapKey(false);
+    if (inputValue.length >= 1 && filteredData.length === 0) {
+      setEmpty(false);
+      setNoData(true);
+      setTapKey(false);
+      setSeeAll(false);
+    } else if (filteredData.length >= 1) {
+      setEmpty(false);
+      setTapKey(true);
+      setSeeAll(false);
+      setNoData(false);
+    }
+  }, [inputValue, filteredData]);
+
+  useEffect(() => {
+    setTapKey(false);
+
     const handleResize = () => {
       if (window.innerWidth <= 812) {
         setData(mediaTransData);
+        setTapKey(false);
       } else if (window.innerWidth >= 813) {
         setData(transactionsData);
+        setTapKey(false);
       }
     };
 
@@ -35,48 +83,15 @@ const InputComp = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    const filter = data.filter((el) => {
-      return el.company.toLowerCase().includes(inputValue.toLowerCase());
-    });
-
-    setFilteredData(filter);
-
-    if (inputValue.length <= 0) {
-      setEmpty(true);
-
-      setTimeout(() => {
-        setEmpty(false);
-      }, 3000);
-    } else if (filteredData.length <= 0) {
-      setTapKey(true);
-      setFilteredData(filter);
-    } else if (inputValue.length >= 1 && filteredData.length <= 0) {
-      setEmpty(false);
-      setTapKey(true);
-      setFilteredData(filter);
-    } else if (inputValue.length >= 1 && filteredData.length !== 0) {
-      setEmpty(false);
-      setTapKey(true);
-      setFilteredData(filter);
-    }
-  }, [inputValue, data]);
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-
-      setInputValue(event.currentTarget.value);
-    }
-  };
-
   const preventDefault = (event: FormEvent) => {
     event.preventDefault();
   };
 
   const handleSeeAll = () => {
     setSeeAll(true);
-    setFilteredData(data);
+    setEmpty(false);
+    setTapKey(false);
+    setNoData(false);
   };
 
   return (
@@ -88,7 +103,6 @@ const InputComp = () => {
             <input
               placeholder="Search by name"
               onKeyDown={handleKeyDown}
-              onClick={() => setInputValue("")}
               className="input__components--input"
               type="text"
             />
@@ -101,12 +115,39 @@ const InputComp = () => {
           </form>
         </div>
       </div>
-      <section className="transactions">
-        {seeAll ? (
-          seeAll && filteredData.length === 0 ? (
-            <p className="transactions__error">No transaction has been found</p>
-          ) : (
-            seeAll &&
+      {
+        <section className="transactions">
+          {noData && <p>No transaction has been found</p>}
+          {empty && <p>Please enter a name</p>}
+          {seeAll &&
+            data.map((el, index) => (
+              <div key={index} className="transactions__flex">
+                <div className="transactions__company">
+                  <div className="transactions__company__arrow">
+                    <img
+                      className="transactions__company__arrow--icon"
+                      src={arrow}
+                      alt="arrow-icon"
+                    />
+                  </div>
+                  <div className="transactions__company__data">
+                    <p className="transactions__company__data--name">
+                      {el.company}
+                    </p>
+                    <p className="transactions__company__data--date">
+                      {el.date}
+                    </p>
+                  </div>
+                </div>
+                <div className="transactions__quantity">
+                  <p className="transactions__quantity--amount">{el.amount}</p>
+                  <p className="transactions__quantity--currency">
+                    To<span> your USD account</span>
+                  </p>
+                </div>
+              </div>
+            ))}
+          {tapKey &&
             filteredData.map((el, index) => (
               <div key={index} className="transactions__flex">
                 <div className="transactions__company">
@@ -133,41 +174,9 @@ const InputComp = () => {
                   </p>
                 </div>
               </div>
-            ))
-          )
-        ) : tapKey && filteredData.length === 0 ? (
-          <p className="transactions__error">No transaction has been found</p>
-        ) : empty ? (
-          <p className="transactions__error">Please enter a name</p>
-        ) : (
-          tapKey &&
-          filteredData.map((el, index) => (
-            <div key={index} className="transactions__flex">
-              <div className="transactions__company">
-                <div className="transactions__company__arrow">
-                  <img
-                    className="transactions__company__arrow--icon"
-                    src={arrow}
-                    alt="arrow-icon"
-                  />
-                </div>
-                <div className="transactions__company__data">
-                  <p className="transactions__company__data--name">
-                    {el.company}
-                  </p>
-                  <p className="transactions__company__data--date">{el.date}</p>
-                </div>
-              </div>
-              <div className="transactions__quantity">
-                <p className="transactions__quantity--amount">{el.amount}</p>
-                <p className="transactions__quantity--currency">
-                  To<span> your USD account</span>
-                </p>
-              </div>
-            </div>
-          ))
-        )}
-      </section>
+            ))}
+        </section>
+      }
     </>
   );
 };
