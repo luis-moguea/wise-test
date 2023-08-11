aee5b124a88ad17e7687f57c7d7aa5bd78c72335
Andres Campuzano Garzon <campuxano88@gmail.com>
2023-08-11 10:12:13 -0500

    refactor: update input logic to handle filters, reduce code

--- src/components/views/InputComp.tsx
+++ src/components/views/InputComp.tsx
@@ -1,9 +1,8 @@
-import { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import "../../styles/input.css";
 import "../../styles/transactions.css";
 import arrow from "../../../public/icons/arrow down.svg";
 import { transactionsData } from "../../data/transactionData";
-import { mediaTransData } from "../../data/mediaTransData";
 
 interface Data {
   company: string;
@@ -12,85 +11,43 @@ interface Data {
 }
 
 const InputComp = () => {
-  const [filteredData, setFilteredData] = useState<Data[]>([]);
-  const [inputValue, setInputValue] = useState("");
-  const [seeAll, setSeeAll] = useState(false);
   const [data, setData] = useState<Data[]>([]);
-  const [showData, setShowData] = useState<boolean | null>(null);
-  const [empty, setEmpty] = useState(false);
-  const [noData, setNoData] = useState(false);
+  const [filteredData, setFilteredData] = useState<Data[]>([]);
+  const [queryName, setQueryName] = useState<string>("");
 
+  /**
+   * Loads the data
+   */
   useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 812) {
-        setData(mediaTransData);
-      } else if (window.innerWidth >= 813) {
-        setData(transactionsData);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
+    setData(transactionsData)
   }, []);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
+  /**
+   * Filters and renders the data
+   */
+  useEffect(() => {
+    // Deep clone
+    let arr = data.map((x) => ({...x}));
 
-      const inputValueTrim = (event.target as HTMLInputElement).value.trim();
-      setInputValue(inputValueTrim);
-
-      if (inputValueTrim.length === 0) {
-        setEmpty(true);
-        setNoData(false);
-        setShowData(null);
-        setSeeAll(false);
-
-        setTimeout(() => {
-          setEmpty(false);
-        }, 3000);
-      } else {
-        const filter = data.filter((el) => {
-          return el.company
-            .toLowerCase()
-            .includes(inputValueTrim.toLowerCase());
-        });
-
-        setFilteredData(filter);
-
-        if (inputValueTrim.length >= 1 && filter.length === 0) {
-          setEmpty(false);
-          setNoData(true);
-          setShowData(false);
-          setSeeAll(false);
-        } else if (filter.length >= 1) {
-          setEmpty(false);
-          setNoData(false);
-          setShowData(true);
-          setSeeAll(false);
-        } else {
-          setEmpty(false);
-          setNoData(false);
-          setShowData(null);
-          setSeeAll(false);
-        }
-      }
+    // Filters by name
+    if (queryName) {
+      arr = arr.filter(item => item.company.toLowerCase().includes(queryName.toLowerCase()));
     }
-  };
+
+    setFilteredData(arr);
+  }, [data, queryName]);
 
   const preventDefault = (event: FormEvent) => {
     event.preventDefault();
   };
 
   const handleSeeAll = () => {
-    setSeeAll(true);
-    setEmpty(false);
-    setShowData(false);
-    setNoData(false);
+    setQueryName("")
   };
+
+  const onQueryNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQueryName(e.target.value.trim())
+  }
 
   return (
     <>
@@ -100,12 +57,14 @@ const InputComp = () => {
           <form onSubmit={preventDefault}>
             <input
               placeholder="Search by name"
-              onKeyDown={handleKeyDown}
+              value={queryName}
+              onChange={onQueryNameChange}
               className="input__components--input"
               type="text"
             />
             <button
-              onClick={() => handleSeeAll()}
+                type="submit"
+              onClick={handleSeeAll}
               className="input__components--button"
             >
               See all
@@ -115,48 +74,16 @@ const InputComp = () => {
       </div>
       {
         <section className="transactions">
-          {noData && (
+          {!filteredData.length && (
             <p>
               No transaction found with company's name
               <span className="transactions__input__value">
                 {" "}
-                "{inputValue}"
+                "{queryName}"
               </span>
             </p>
           )}
-          {empty && (
-            <p className="transactions__input__empty">Please enter a name</p>
-          )}
-          {seeAll &&
-            data.map((el, index) => (
-              <div key={index} className="transactions__flex">
-                <div className="transactions__company">
-                  <div className="transactions__company__arrow">
-                    <img
-                      className="transactions__company__arrow--icon"
-                      src={arrow}
-                      alt="arrow-icon"
-                    />
-                  </div>
-                  <div className="transactions__company__data">
-                    <p className="transactions__company__data--name">
-                      {el.company}
-                    </p>
-                    <p className="transactions__company__data--date">
-                      {el.date}
-                    </p>
-                  </div>
-                </div>
-                <div className="transactions__quantity">
-                  <p className="transactions__quantity--amount">{el.amount}</p>
-                  <p className="transactions__quantity--currency">
-                    To<span> your USD account</span>
-                  </p>
-                </div>
-              </div>
-            ))}
-          {showData &&
-            filteredData.map((el, index) => (
+          {filteredData.map((el, index) => (
               <div key={index} className="transactions__flex">
                 <div className="transactions__company">
                   <div className="transactions__company__arrow">
